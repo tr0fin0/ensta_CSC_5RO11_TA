5a510c4c7a4c42bdaed8933ec06a3d682b264710
Guilherme Nunes Trofino <guitrofino@gmail.com>
2024-09-29 16:54:57 +0200

    [+] homework_1 initial files added

---
+++ build/homework_1/catkin_generated/installspace/node_A.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+message = "secret"
+
+def callback(data):
+    rospy.loginfo(f"Node A received: {data.data}")
+
+def talker():
+    pub = rospy.Publisher('outgoing_A', String, queue_size=10)
+    rate = rospy.Rate(0.5)  # 1 message every 2 seconds
+    while not rospy.is_shutdown():
+        pub.publish(message)
+        rate.sleep()
+
+def listener():
+    rospy.Subscriber('incoming_A', String, callback)
+
+if __name__ == '__main__':
+    rospy.init_node('node_A', anonymous=True)
+    listener()
+    talker()
---
+++ build/homework_1/catkin_generated/stamps/homework_1/node_B.py.stamp
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(f"Node B received: {data.data}")
+    new_msg = data.data + "B"
+    pub.publish(new_msg)
+
+def listener():
+    rospy.Subscriber('outgoing_A', String, callback)
+
+if __name__ == '__main__':
+    rospy.init_node('node_B', anonymous=True)
+    pub = rospy.Publisher('outgoing_B', String, queue_size=10)
+    listener()
+    rospy.spin()
